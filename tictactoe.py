c3fc319077c73ad755031be553291c14f923554d
Isaac <it238@byu.edu>
2023-08-03 10:42:13 -0400

    implemented array and printBoard()

---
+++ tictactoe.py
@@ -0,0 +1,41 @@
+# started at 10:30am on 8/3/2023
+
+'''
+Okay, so I'm going to try to make a tic tac toe game in python. I think I will use a 2d array  to represent the board.
+'''
+
+array = [[0,0,0],[0,0,0],[0,0,0]]
+
+'''
+I'm gonna have a function that prints the board, a function that checks if someone has won, and a function that checks if there is a tie. Also, I might as well make a function that plays the game.
+'''
+
+def printBoard():
+    print(array[0])
+    print(array[1])
+    print(array[2])
+
+'''
+Simple enough. I might come back and fix up the formatting later, but for now, this is fine.
+'''
+
+def checkWin():
+    # unimplemented
+    return
+
+def checkTie():
+    # unimplemented
+    return
+
+def playGame():
+    # unimplemented
+    if checkWin():
+        return
+    if checkTie():
+        return
+    return
+
+def main():
+    return
+
+main()
