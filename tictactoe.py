5e9202052161d5af59f309d7f7c631e228d491c4
Isaac <it238@byu.edu>
2023-08-03 11:20:21 -0400

    started an implementation of playGame().

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,16 @@
 # started at 10:30am on 8/3/2023
 
+from random import randint
+
 '''
-Okay, so I'm going to try to make a tic tac toe game in python. I think I will use a 2d array  to represent the board.
+Okay, so I'm going to try to make a tic tac toe game in python. I think I will use a 2d array  to represent the board. I'll also make a list of players, the first one being the player (X) and the second one being the computer (O).
 '''
 
 blank_array = [['_','_','_'],['_','_','_'],['_','_','_']]
+players = ['X','O']
           
 '''
-I'm gonna have a function that prints the board, a function that checks if the player (X) has won, and a function that checks if there is a tie. Also, I might as well make a function that plays the game.
+I'm gonna have a function that prints the board, a function that checks if the player has won, and a function that checks if there is a tie.
 '''
 
 def printBoard(array):
@@ -18,8 +21,6 @@ def printBoard(array):
 '''
 Simple enough. I might come back and fix up the formatting later, but for now, this is fine.
 '''
-
-players = ['X','O']
 
 def checkWin(array, player):
     for i in range(3):
@@ -37,28 +38,77 @@ def checkWin(array, player):
         return True
     return False
 
-def checkTie():
-    # unimplemented
-    return
+'''
+Okay, I'm satisfied with this. Let's do the tie and result function.
+'''
+
+def checkTie(array):
+    # if all spaces are filled
+    if '_' not in array[0] and '_' not in array[1] and '_' not in array[2]:
+        return True
+
+def checkResult():
+    for player in players:
+        if checkWin(player):
+            return player
+    if checkTie():
+        return 'Tie'
+    return None
+
+'''
+Now, I need a function that takes in the player's input and updates the board.
+'''
 
 def playGame():
-    # unimplemented
-    if checkWin():
+    print('Welcome to Tic Tac Toe!')
+    print('You are X, and the computer is O.')
+    print('To play, enter the row and column of the space you want to play in.')
+    print('The top left space is 0,0, and the bottom right space is 2,2.')
+    print('Good luck!\n')
+    printBoard(blank_array)
+    while True:
+        guess = input('Enter your guess: ')
+        guess = guess.split(',')
+        guess = [int(guess[0]), int(guess[1])]
+        if blank_array[guess[0]][guess[1]] == '_':
+            blank_array[guess[0]][guess[1]] = 'X'
+            break
+        else:
+            print('That space is already taken. Try again.')
+    printBoard(blank_array)
+    if checkWin(blank_array, 'X'):
+        print('You win!')
         return
-    if checkTie():
+    if checkTie(blank_array):
+        print('Tie!')
         return
-    return
+    print('Computer\'s turn...')
+    while True:
+        guess = [randint(0,2), randint(0,2)]
+        if blank_array[guess[0]][guess[1]] == '_':
+            blank_array[guess[0]][guess[1]] = 'O'
+            break
+    printBoard(blank_array)
+    if checkWin(blank_array, 'O'):
+        print('You lose!')
+        return
+    if checkTie(blank_array):
+        print('Tie!')
+        return
+    playGame()
 
 def main():
-    game1 = [['X','X','X'],['_', '_', '_'], ['_', '_', '_']]
-    print(checkWin(game1, 'X'))
-    game2 = [['X','_','_'],['_', 'X', '_'], ['_', '_', 'X']]
-    print(checkWin(game2, 'X'))
-    game3 = [['_','O','_'],['_', 'O', '_'], ['_', 'O', '_']]
-    print(checkWin(game3, 'O'))
-    game4 = [['_','_','O'],['_', 'O', '_'], ['O', '_', '_']]
-    print(checkWin(game4, 'O'))
-    game5 = [['_','_','O'],['_', 'X', '_'], ['O', '_', '_']]
-    print(checkWin(game5, 'O'))
+    # game1 = [['X','X','X'],['_', '_', '_'], ['_', '_', '_']]
+    # print(checkWin(game1, 'X'))
+    # game2 = [['X','_','_'],['_', 'X', '_'], ['_', '_', 'X']]
+    # print(checkWin(game2, 'X'))
+    # game3 = [['_','O','_'],['_', 'O', '_'], ['_', 'O', '_']]
+    # print(checkWin(game3, 'O'))
+    # game4 = [['_','_','O'],['_', 'O', '_'], ['O', '_', '_']]
+    # print(checkWin(game4, 'O'))
+    # game5 = [['_','_','O'],['_', 'X', '_'], ['O', '_', '_']]
+    # print(checkWin(game5, 'O'))
+
+    playGame()
 
 main()
