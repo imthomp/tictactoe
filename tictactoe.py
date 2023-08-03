ff8d8538e188bc3d3925d63d58a90bdcbc5ba5b7
Isaac <it238@byu.edu>
2023-08-03 11:01:25 -0400

    implemented and tested checkWin()

--- tictactoe.py
+++ tictactoe.py
@@ -4,13 +4,13 @@
 Okay, so I'm going to try to make a tic tac toe game in python. I think I will use a 2d array  to represent the board.
 '''
 
-array = [[0,0,0],[0,0,0],[0,0,0]]
-
+blank_array = [['_','_','_'],['_','_','_'],['_','_','_']]
+          
 '''
-I'm gonna have a function that prints the board, a function that checks if someone has won, and a function that checks if there is a tie. Also, I might as well make a function that plays the game.
+I'm gonna have a function that prints the board, a function that checks if the player (X) has won, and a function that checks if there is a tie. Also, I might as well make a function that plays the game.
 '''
 
-def printBoard():
+def printBoard(array):
     print(array[0])
     print(array[1])
     print(array[2])
@@ -19,9 +19,23 @@ def printBoard():
 Simple enough. I might come back and fix up the formatting later, but for now, this is fine.
 '''
 
-def checkWin():
-    # unimplemented
-    return
+players = ['X','O']
+
+def checkWin(array, player):
+    for i in range(3):
+        # horizontal
+        if array[i][0] == array[i][1] == array[i][2] == player:
+            return True
+        # vertical
+        if array[0][i] == array[1][i] == array[2][i] == player:
+            return True
+    # downward
+    if array[0][0] == array[1][1] == array[2][2] == player:
+        return True
+    # upward
+    if array[0][2] == array[1][1] == array[2][0] == player:
+        return True
+    return False
 
 def checkTie():
     # unimplemented
@@ -36,6 +50,15 @@ def playGame():
     return
 
 def main():
-    return
+    game1 = [['X','X','X'],['_', '_', '_'], ['_', '_', '_']]
+    print(checkWin(game1, 'X'))
+    game2 = [['X','_','_'],['_', 'X', '_'], ['_', '_', 'X']]
+    print(checkWin(game2, 'X'))
+    game3 = [['_','O','_'],['_', 'O', '_'], ['_', 'O', '_']]
+    print(checkWin(game3, 'O'))
+    game4 = [['_','_','O'],['_', 'O', '_'], ['O', '_', '_']]
+    print(checkWin(game4, 'O'))
+    game5 = [['_','_','O'],['_', 'X', '_'], ['O', '_', '_']]
+    print(checkWin(game5, 'O'))
 
 main()
